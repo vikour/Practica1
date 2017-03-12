20e9cc3b40c634d4057f740087c9bfb127927fe8
Juan Palma Borda <juanpalmaborda@hotmail.com>
2017-03-12 20:08:10 +0100

    Tercer Intento, ya modificando el java

--- GitAux.java
+++ GitAux.java
@@ -9,15 +9,15 @@ package prgitejemplo;
  * @author bertoa
  */
 public class GitAux {
-    
+
     public void metodoAlumno1(){
         System.out.println("metodo 1");
     }
-    
+    //Comentario hecho por Juan Palma Borda
     public void metodoAlumno2(){
-        System.out.println("metodo 2");
+        System.out.println("Aqui estuvo Juan Palma Borda");
     }
-    
+
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
     }
